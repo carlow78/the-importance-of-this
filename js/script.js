e5f5557a7cdae3f7f16ffa82b60229f160715825
John Lynch <johnf.lynch@yahoo.ie>
2024-05-23 13:56:50 +0000

    Update 3 video completion

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
     $(".box").on("click", function(){
-        var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
-    });
-});
+        
+  
 
 /**
  * When we click on an element that has
  * a 'box' class, this event will be fired.
  */
-
+    var classNames = $(this).attr("class").split(" ");
+    var boxClass = classNames[0];
+    var classNames = classNames[1];
+    if ($(this).css("background-color") == "rgb(255,0,0)"){
+        // If this object is already red, turn it black
+        $("." + classNames).css("background-color", "#000");
+    }else{
+        /**
+         * Else turn all elements with a box class black
+         * and then change the color of all elements
+         * with the same class name as the clicked element
+         * to red.
+         */
+        $("." + boxClass).css("background-color", "#000");
+        $("." + classNames).css("background-color", "red");
+    }
+    });
+})
