43814cfb5e1a7d11ab81353ec460ac5348627d78
John Lynch <johnf.lynch@yahoo.ie>
2024-05-23 13:47:06 +0000

    update 2 video

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-$('button').mouseenter(function(){
-    $(this).removeClass('makeRed').addClass('makeBlue');
+$(document).ready(function(){
+    $(".box").on("click", function(){
+        var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
 });
 
-$('button').mouseleave(function(){
-    $(this).removeClass('makeBlue').addClass('makeRed');
-});
+/**
+ * When we click on an element that has
+ * a 'box' class, this event will be fired.
+ */
+
